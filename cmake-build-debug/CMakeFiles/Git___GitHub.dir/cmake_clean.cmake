04e0b7fa642213ef7851d2553fdfd70985d6fc71
Subhrakamal Das <jyb2084.usa@gmail.com>
2022-01-22 16:36:20 +0530

    HI Everyone

---
+++ cmake-build-debug/CMakeFiles/Git___GitHub.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Git___GitHub.dir/Subhro.cpp.obj"
+  "Git___GitHub.exe"
+  "Git___GitHub.exe.manifest"
+  "Git___GitHub.pdb"
+  "libGit___GitHub.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Git___GitHub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
